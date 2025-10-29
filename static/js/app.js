64f90652b3dcff47797d401836007c43a8b5e75e
OmarCasH <70552508+OmarCasH@users.noreply.github.com>
2025-10-29 09:27:10 -0500

    Actualizacion encuesta app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,36 @@
 // ---- sidebar menu ------
 
-const botones = document.querySelectorAll('.link__sidebar'); // Selecciona todos los botones del menu
-const secciones = document.querySelectorAll('.seccion__menu'); // Selecciona todas las secciones de contenido
+const botones = document.querySelectorAll('.link__sidebar'); 
+const secciones = document.querySelectorAll('.seccion__menu'); 
 
 botones.forEach(boton => {
   boton.addEventListener('click', function (event) {
-    console.log("prueba boton")
-    event.preventDefault(); // Evita que el enlace <a href=""> recargue la página
+    event.preventDefault(); 
 
-    // Oculta todas las secciones
     secciones.forEach(sec => sec.classList.remove('activa'));
 
-    // Obtiene el nombre de la sección a mostrar
     const target = this.getAttribute('data-seccion');
 
-    // Muestra solo la sección correspondiente
     const seccionMostrar = document.querySelector(`[data-content="${target}"]`);
 
     seccionMostrar.classList.add('activa');
+  });
+});
+
+// --------- BTN ENCUESTA ----------
+
+const botonesEncuesta = document.querySelectorAll('.btn__encuesta');
+
+botonesEncuesta.forEach(boton => {
+  boton.addEventListener('click', function (event) {
+    event.preventDefault(); 
+
+    botonesEncuesta.forEach(b => {
+      b.style.backgroundColor = "";
+      b.style.color = "";
+    })
+
+    boton.style.backgroundColor = "pink";
+    boton.style.color = "black";
   });
 });
